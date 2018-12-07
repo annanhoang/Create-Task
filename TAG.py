517faefe219e10576c4810cef69b08875588edda
annanhoang <45671411+annanhoang@users.noreply.github.com>
2018-12-07 13:01:29 -0800

    Add files via upload

--- TAG.py
+++ TAG.py
@@ -6,26 +6,61 @@ def dead():
 def invalid():
     print("That isn't an option! Try again!")
 
-yes_or_no = ["yes", "no"]
-directions = ["left", "right", "forward",]
 
 
 # start with introduction
 print("You've been magically transported to the World of Oz." )
-print("You must find the ruby shoes to return home but if you die you're stuck in this world forever")
-print("You walk along the golden brick road and see that it divides into two paths.")
-print("The path on the left looks dark, gloomy, and worn down. ")
-print("The right one is less worn but nicer.")
+print("You must find the ruby shoes and help out some others in order to return home but if you die you're stuck in this world forever")
 #road = int(input("Enter left for the worn down path or 2 for the nicer path: "))
 
-
-response = ""
-while response not in directions:
-    response = input("Do you choose the left path or the right path?")
+def road():
+    print("You walk along the golden brick road and see that it divides into two paths.")
+    print("The path on the left looks dark, gloomy, and worn down. ")
+    print("The right one is less worn but nicer.")
+    response = input("Do you choose the left path or the right path? ")
 #what happens when each path is chosen
 #trying to fix bc it always goes to the option of left
-    print("You head down the left path, you end up tripping and hitting your head on a rock.")
-    print("You head down the right path, you make it to the other side and see a scarecrow.")
+    if response == "left" or response == "Left":
+        print("You trip on a rock and hit your head.")
+        dead()
+    elif response == "right" or response == "Right":
+        print("You make it to the other side and see a field of corn.")
+    else:
+        invalid()
+        road()
+road()
+
+def scarecrow_brain():
+    print("As you walk through the field you see a tall shadow.")
+    print("It comes towards you and you see a large scarecrow!")
+    print("He kindly asks you to help him find a brain and you say yes!")
+    print("In order to find his brain you must go to the center of the maze.")
+    print("You keep heading forward and hit a wall, the only way to go is right.")
+    print("You go straight, it divides into a path on the left and right")
+    response = input("Left or right? ")
+    if response == "right" or response == "Right":
+        print("The right path leads to a dead end! You head back and go left instead.")
+    else:
+        print("You keep heading down the left path until you hit a dead and turn right.")
+        print("This way leads to another diversion, left or right? ")
+        if response == "left" or response == "Left":
+            print("You hit a dead end and head back the other way.")
+        elif response == "right" or response == "Right":
+            response = input("You keep walking and turn with the corner. "
+                  "As you keep walking you can either turn right or go forward. ")
+            if response == "right" or "Right":
+                print("You walk down the right path and follow the only turns until you see a huge sqare area with a chest!")
+                print("You open the chest and find the scarecrow's brain in a jar!")
+                print("You give it back to him and he promises to help you find your ruby shoes.")
+            else:
+                return 0
+
+
+
+
+
+
+
 # if response == "left" or "Left":
 #     print("As you walk along the path you trip on the uneven bricks")
 #     dead()
